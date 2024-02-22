f3bb9cb5157fa48380a3dc079eb2a4da5c9b22ac
natlopar <natalialopezariel@gmail.com>
2024-02-22 11:22:16 +0100

    motor plantillas

--- node-ejercicio-netflix/src/index.js
+++ node-ejercicio-netflix/src/index.js
@@ -5,7 +5,8 @@ const mysql = require ('mysql2/promise');
 // create and config server
 const server = express();
 server.use(cors());
-server.use(express.json({limit: "25mb"}));
+server.use(express.json());
+server.set('view engine', 'ejs');
 
 // init express aplication
 const serverPort = 4000;
@@ -18,7 +19,7 @@ async function getConnection() {
   const connection = await mysql.createConnection({
     host: 'localhost',
     user: 'root', 
-    password: 'adalab2024', 
+    password: 'root24', 
     database: 'netflix'
   });
   connection.connect();
@@ -26,15 +27,28 @@ async function getConnection() {
 };
 
 //endpoints
+server.get('/movie/:movieId', async (req, res) => { 
+  console.log(req.params.movieId);
+  const idFilterParam = req.params.movieId;
+  const conex= await getConnection();
+  const sql = 'SELECT * FROM movies WHERE idMovies = ?';
+  const [results, fields] = await conex.query(sql, [idFilterParam]);
+  
+  res.render('movies', {movies: results[0]} );
+  console.log(results);  
+  });
+
 
 server.get('/api/movies', async (req,res) => {
-  console.log(req.query.genre)
   const genreFilterParam = req.query.genre;
   const conex= await getConnection();
   const sql = 'SELECT * FROM movies WHERE genre = ?';
   const [results, fields] = await conex.query(sql, [genreFilterParam]);
-  console.log(results);
-  console.log(fields);
   res.json({success: true, movies: results});
-  console.log(res);
-})
+});
+
+
+
+
+const staticServerPathWeb = './src/public-react'; // En esta carpeta ponemos los ficheros estáticos
+server.use(express.static(staticServerPathWeb));
